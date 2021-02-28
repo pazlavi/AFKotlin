2697faeb3396c67196af5969a92288003ca76db5
pazlavi <paz.lavi@appsflyer.com>
2021-02-28 16:23:47 +0200

    Initial commit

---
+++ app/src/main/java/com/appsflyer/afexample1/DeferredDeepLinkActivity.kt
@@ -0,0 +1,11 @@
+package com.appsflyer.afexample1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeferredDeepLinkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deferred_deep_link)
+    }
+}
